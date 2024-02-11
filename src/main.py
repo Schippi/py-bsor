803918651b38c7fdbb4286f3b5588bf98e4f24be
Schippi <theschippi@gmail.com>
2024-02-11 10:21:16 +0100

    some util functions
    download_Bsor(id) -> Bsor
    clan_playlist(clan: str, count: int = 20, imageb64: str = None, unplayed_player: int = None, stars_from: int = None, stars_to: int = None, include_to_hold: bool = False) -> dict:

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
 from bsor.Bsor import make_bsor
 from bsor.Scoring import calc_stats
+import logging
 
 if __name__ == '__main__':
     import os
+    logging.basicConfig()
+    logging.getLogger().setLevel(logging.INFO)
+    logging.info('Started')
 
     # example, read basic info from bsor file
     filename = 'D:/_TMP/easy.bsor'
@@ -13,3 +17,12 @@ if __name__ == '__main__':
         print(f'BSOR Version:  {m.file_version}')
         print(f'BSOR notes: {len(m.notes)}')
         print(f'BSOR stats: {stats}')
+
+    from bsor.BsorUtil import *
+    #my_score = download_Bsor(787433)
+    #print(my_score)
+
+    my_playlist = clan_playlist('GER', unplayed_player=76561198026425351, stars_from=3, stars_to=9, include_to_hold=False);
+    with open('D:/_TMP/GER.bplist', 'w') as f:
+        f.write(json.dumps(my_playlist, indent=2))
+    print(my_playlist)
